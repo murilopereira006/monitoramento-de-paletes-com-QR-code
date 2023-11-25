522b0f6214ac08a0a6cc00dae81b2807431d0792
muriloPereira006 <muriloedu006@gmail.com>
2023-11-24 21:16:25 -0300

    abertura de camera e captura de rostos @

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print('OpenCv')
+import cv2 as cv
+import mediapipe as mp
+
+webcam = cv.VideoCapture(0)
+
+reconhecimento_rosto = mp.solutions.face_detection
+desenho = mp.solutions.drawing_utils
+reconhecedor_rosto = reconhecimento_rosto.FaceDetection()
+
+while webcam.isOpened():
+    validacao, frame = webcam.read()
+    if not validacao:
+        break
+    imagem = frame
+    lista_rostos = reconhecedor_rosto.process(imagem)
+    
+    if lista_rostos.detections:
+        for rosto in lista_rostos.detections:
+            desenho.draw_detection(imagem, rosto)
+    
+    cv.imshow("Rostos na sua webcam", imagem)
+    if cv.waitKey(5) == 27:
+        break
+webcam.release()
+cv.destroyAllWindows()
