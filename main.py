be14eba47ff94ba17e8470b64f548f31afb10b61
marcelo <marceloishida14@gmail.com>
2023-11-24 23:35:04 -0300

    leitura de qr code

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
-import cv2 as cv
-import mediapipe as mp
+import cv2
+from pyzbar.pyzbar import decode
 
-webcam = cv.VideoCapture(0)
+# Inicialização da captura da webcam
+webcam = cv2.VideoCapture(0)
 
-reconhecimento_rosto = mp.solutions.face_detection
-desenho = mp.solutions.drawing_utils
-reconhecedor_rosto = reconhecimento_rosto.FaceDetection()
+if not webcam.isOpened():
+    print("Erro ao acessar a câmera. Certifique-se de que a câmera está conectada e funcionando corretamente.")
+else:
+    print("Câmera acessada com sucesso. Pressione 'Esc' para sair.")
 
-while webcam.isOpened():
+while True:
     validacao, frame = webcam.read()
     if not validacao:
+        print("Erro ao ler a câmera.")
         break
-    imagem = frame
-    lista_rostos = reconhecedor_rosto.process(imagem)
-    
-    if lista_rostos.detections:
-        for rosto in lista_rostos.detections:
-            desenho.draw_detection(imagem, rosto)
-    
-    cv.imshow("Rostos na sua webcam", imagem)
-    if cv.waitKey(5) == 27:
+
+    # Decodificar QR codes na imagem da webcam
+    decoded_objects = decode(frame)
+    for obj in decoded_objects:
+        # Obter os pontos do QR code como um retângulo
+        rect_pts = obj.rect
+        cv2.rectangle(frame, (rect_pts[0], rect_pts[1]), (rect_pts[0] + rect_pts[2], rect_pts[1] + rect_pts[3]), (0, 255, 0), 2)
+
+        # Mostrar o conteúdo do QR code na tela
+        cv2.putText(frame, "Link: " + str(obj.data.decode('utf-8')), (rect_pts[0], rect_pts[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+    cv2.imshow("Leitor de QR Code", frame)
+
+    # Aguardar 1 milissegundo e verificar se a tecla 'Esc' foi pressionada
+    key = cv2.waitKey(1)
+    if key == 27:
+        print("Programa encerrado pelo usuário.")
         break
+
+# Liberar a captura da webcam e fechar as janelas
 webcam.release()
-cv.destroyAllWindows()
+cv2.destroyAllWindows()
