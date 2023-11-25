4eaa3fbf8271969b556726ace6d79015ab149c39
muriloPereira006 <muriloedu006@gmail.com>
2023-11-25 12:06:39 -0300

    identificacao de posicao do QR code

--- main.py
+++ main.py
@@ -11,17 +11,8 @@ else:
 while True:
     validacao, frame = webcam.read()
 
-
-
-
-     
-    width  = webcam.get(3)   # float `width`
-    height = webcam.get(4)  # float `height`
-    print(width)
-    print(height)
-
-
-
+    frameWidth  = webcam.get(3)   # float `frameWidth`640 / 3 = 212
+    height = webcam.get(4)  # float `height`480 / 3 = 160
 
     if not validacao:
         print("Erro ao ler a câmera.")
@@ -30,10 +21,13 @@ while True:
     decoded_objects = decode(frame)
     for obj in decoded_objects:
         rect_pts = obj.rect
-        print(rect_pts[0])
-        cv2.rectangle(frame, (rect_pts[0], rect_pts[1]), (rect_pts[0] + rect_pts[2], rect_pts[1] + rect_pts[3]), (0, 255, 0), 2)
-
-        cv2.putText(frame, "-> " + str(obj.data.decode('utf-8')), (rect_pts[0] + rect_pts[2] + 10, rect_pts[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        
+        if rect_pts[0] > frameWidth:
+            cv2.rectangle(frame, (rect_pts[0], rect_pts[1]), (rect_pts[0] + rect_pts[2], rect_pts[1] + rect_pts[3]), (0, 255, 0), 2)
+            cv2.putText(frame, "-> " + str(obj.data.decode('utf-8')), (rect_pts[0] + rect_pts[2] + 10, rect_pts[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        else:
+            cv2.rectangle(frame, (rect_pts[0], rect_pts[1]), (rect_pts[0] + rect_pts[2], rect_pts[1] + rect_pts[3]), (255, 0, 0), 2)
+            cv2.putText(frame, "-> " + str(obj.data.decode('utf-8')), (rect_pts[0] + rect_pts[2] + 10, rect_pts[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
 
     cv2.imshow("Leitor de QR Code", frame)
 
